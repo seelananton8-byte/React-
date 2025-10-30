159da8cd897a75678965fa4e0c181c88b7f80e08
seelananton8-byte <seelananton8@gmail.com>
2025-10-30 13:20:25 +0530

    Update Ant.jsx

--- Ant.jsx
+++ Ant.jsx
@@ -1,1 +1,12 @@
+function Ant(){
+    const name = 'Anton Sureshkumar';
+    const age = 23;
+    
+    return(
+        <h1>
+            Hi! My Name is {name} and My Age is {age}
+        </h1>
+    );
+}
+export default Ant
 
