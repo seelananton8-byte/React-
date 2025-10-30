9987c06d325b9f79e6c24a69a5829daaa524788a
seelananton8-byte <seelananton8@gmail.com>
2025-10-30 11:40:18 +0530

    New one

--- About.js
+++ About.js
@@ -1,1 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
+const el= document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+function App(){
+    return <h1>Hi! How are you?</h1>;
+}
+
+root.render(<App/>);
